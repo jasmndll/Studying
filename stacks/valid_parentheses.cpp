5c85b99e40d624280677de05c8a7e41cec9c3fb0
jasmndll <jddalal790@gmail.com>
2025-12-27 14:10:33 +0530

    Changes

--- stacks/valid_parentheses.cpp
+++ stacks/valid_parentheses.cpp
@@ -4,8 +4,34 @@ using namespace std;
 
 class Stack{
 public: 
-    stack<int> st;
-    void check(stack<int>& st){
-     
+    bool check(string s){
+        stack<int> st;
+        for(char x : s){
+            if( x== '(' || x == '{' || x == '['){
+                st.push(x);
+            }
+            else{
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if((x == ')' && top != '(' ) || (x == '}' && top != '{') || (x == ']' && top !='[')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
     }
 };
+
+
+int main() {
+    Stack sk;
+    string s = "[({})]";
+    if(sk.check(s)){
+        cout<<"Valid palindrome."<<endl;
+    }
+    else{
+        cout<<"Invalid.";
+    }
+    return 0;
+}
