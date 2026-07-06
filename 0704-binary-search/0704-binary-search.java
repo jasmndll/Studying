6ab21352cc7284a00c15204e9702be381444d162
Jasmine Dalal <jddalal790@gmail.com>
2026-07-06 10:20:01 +0530

    Time: 0 ms (100%), Space: 48.5 MB (31.13%) - LeetHub - Time: O, Space: O(?)

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int st = 0;
+        int end = n-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
