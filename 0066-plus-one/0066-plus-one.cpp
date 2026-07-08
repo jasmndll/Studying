2d9142477b9e09c2a59d8941be60ac2582827681
Jasmine Dalal <jddalal790@gmail.com>
2026-07-08 14:22:50 +0530

    Time: 0 ms (100%), Space: 11.3 MB (95.38%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        vector<int> ans;
+        for(int i = 0; i<n; i++){
+            if( i == n-1){
+                ans.push_back(digits[i]+1);
+            }
+            else{
+                ans.push_back(digits[i]);
+            }
+        }
+        for(int i = n-1; i>0; i--){
+            if(ans[i] == 10){
+                ans[i] = 0;
+                ans[i-1] += 1;
+            }
+        }
+        if(ans[0] == 10){
+            ans[0] = 0;
+            ans.insert(ans.begin(), 1);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
