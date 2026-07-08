d834ce5e866da3299ce17c501acfb02c8ad86ac0
Jasmine Dalal <jddalal790@gmail.com>
2026-07-08 12:30:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (72.77%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        int ans = dividend/divisor;
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
