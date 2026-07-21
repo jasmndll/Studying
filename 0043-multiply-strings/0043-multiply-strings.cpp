1e291a8069eca1de5df2d2d8842554dcff20ec95
Jasmine Dalal <jddalal790@gmail.com>
2026-07-21 12:18:36 +0530

    Time: 0 ms (100%), Space: 9.4 MB (88.51%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0"){
+            return "0";
+        }
+        vector<int> res(num1.size()+num2.size(),0);
+        for(int i = num1.size()-1; i>= 0; i--){
+            for(int j = num2.size()-1; j>=0; j--){
+                int sum = (num1[i] - '0')*(num2[j]-'0')+res[i+j+1];
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
+            }
+        }
+            string ans = "";
+            for(int x:res){
+                if(!(ans.empty()&& x==0)){
+                    ans.push_back(x+'0');
+                }
+            }
+        return ans;
+    }
+};
