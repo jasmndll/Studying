d6f2280a4196ec470b980c27ce2d6c7bd7ca1cca
Jasmine Dalal <jddalal790@gmail.com>
2026-07-22 10:52:42 +0530

    Time: 271 ms (39.17%), Space: 66.8 MB (90.06%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    unique_salaries = employee['salary'].drop_duplicates().sort_values(ascending = False)
+    if len(unique_salaries) <2:
+        return pd.DataFrame({'SecondHighestSalary':[None]})
+    second_highest = unique_salaries.iloc[1]
+    return pd.DataFrame({'SecondHighestSalary' : [second_highest]})
+
+    
